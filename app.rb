21ec71208e88d423ea6ffaceecfbc8cbf1af11b9
matochondrion <matochondrion@gmail.com>
2019-09-02 03:03:38 +0000

    add app.rb

---
+++ app.rb
@@ -0,0 +1,114 @@
+require 'sinatra'
+require "sinatra/reloader" if development?
+require "sinatra/json"
+require 'erubis'
+require 'http'
+
+RPSLS_WINNER_SERVER = 'http://http://54.70.36.146:4568'
+CHOICES = [
+  {
+    'choice': {
+      'id': 1,
+      'name': 'rock'
+    }
+  },
+  {
+    'choice': {
+      'id': 2,
+      'name': 'paper'
+    }
+  },
+  {
+    'choice': {
+      'id': 3,
+      'name': 'scissors'
+    }
+  },
+  {
+    'choice': {
+      'id': 4,
+      'name': 'lizard'
+    }
+  },
+  {
+    'choice': {
+      'id': 5,
+      'name': 'spock'
+    }
+  }
+]
+
+configure do
+  set :bind, '0.0.0.0'
+  set :port, '4567'
+end
+
+helpers do
+end
+
+# don't need?
+def play_against_computer(player_choice)
+  get_winner player_choice, computer_choice
+end
+
+def computer_choice_id
+  case retrieve_random_number
+  when 1..20 then 1
+  when 21..40 then 2
+  when 41..60 then 3
+  when 61..80 then 4
+  when 81..100 then 5
+  else
+    raise 'random_number error'
+  end
+end
+
+def retrieve_random_number
+  # TODO: build in some error checking
+  # https://github.com/httprb/http/wiki/Redirects
+  #
+  response = HTTP.accept(:json).follow(max_hops: 3)
+                 .get('http://codechallenge.boohma.com/random')
+  json = JSON.parse response.body
+  json['random_number']
+end
+
+def get_winner(player_choice_id, computer_choice_id)
+  #
+  response = HTTP.accept(:json).follow(max_hops: 3)
+                 .get(RPSLS_WINNER_SERVER)
+  computer_choice_id
+  'win'
+end
+
+get '/' do
+  erb :index
+end
+
+get '/choices' do
+  content_type :json
+  CHOICES.to_json
+end
+
+get '/choice' do
+  content_type :json
+  {
+    "id": 2,
+    "name": 'paper'
+  }.to_json
+end
+
+post '/play' do
+  content_type :json
+
+  game_data = {
+    results: 'tbd',
+    player: 2,
+    computer: computer_choice_id
+  }
+
+  winner = get_winner game_data[:player], game_data[:computer]
+
+  game_data['results'] = winner
+  game_data.to_json
+end
